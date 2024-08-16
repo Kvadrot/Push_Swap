c472d3a35e2ea3f2fc305df80dd5f3fc68010e72
Illia Tykhonov <itykhono@c3r9s3.42warsaw.pl>
2024-08-16 16:06:56 +0200

    ft_sort_five is fixed

--- sort_small_list.c
+++ sort_small_list.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 19:02:46 by itykhono          #+#    #+#             */
-/*   Updated: 2024/08/13 18:12:22 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/08/16 16:06:22 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ void	ft_sort_five(t_nd **origin_list_a, t_nd **origin_list_b)
 	t_nd	*min_t_nd;
 	int		diff;
 
+	diff = 0;
+	min_t_nd = NULL;
 	while (ft_list_length(*origin_list_a) > 3)
 	{
 		min_t_nd = ft_find_min(origin_list_a);
@@ -54,7 +56,8 @@ void	ft_sort_five(t_nd **origin_list_a, t_nd **origin_list_b)
 	}
 	if ((*origin_list_b)->number < (*origin_list_b)->next->number)
 		ft_swap(origin_list_b, "sb");
-	ft_sort_three(origin_list_a);
+	if (ft_is_sorted(origin_list_a, true) == false)
+		ft_sort_three(origin_list_a);
 	ft_push(origin_list_b, origin_list_a, "pa\n");
 	ft_push(origin_list_b, origin_list_a, "pa\n");
 }
